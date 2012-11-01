4de307b89075136ef2330f5904341fe5cc55470c
nomospace <jinlu_hz@163.com>
2012-11-01 15:33:23 +0800

    add android-bootstrap

---
+++ gen/com/sysdev/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.sysdev;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- test/index.js
+++
@@ -1,7 +0,0 @@
-var rf = require('./remote-file');
-var http = require('http');
-
-rf.read('http://s3.ph.126.net/95AxmoHspNEBjGsp_hTIDg==/644859171652174479.js', 'test/data',
-  function(data, error) {
-    console.log(data, error);
-  });
